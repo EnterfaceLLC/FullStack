c941a4410c371560e12ffd6b1f30360f9fa139fe
EnterfaceLLC <dotenterface@gmail.com>
2024-10-12 21:40:07 -0500

    Drizzle Set-up!

---
+++ API/src/db/productsSchema.ts
@@ -0,0 +1,15 @@
+import {
+  doublePrecision,
+  integer,
+  pgTable,
+  text,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const productsTable = pgTable("products", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  price: doublePrecision().notNull(),
+  description: text(),
+  image: varchar({ length: 255 }),
+});
