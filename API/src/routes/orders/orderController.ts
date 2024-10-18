2bd35b2c87880ab0a2354f2e6e2b250e1647c7eb
EnterfaceLLC <dotenterface@gmail.com>
2024-10-17 20:15:35 -0500

    PROD Build update

--- API/src/routes/orders/orderController.ts
+++ API/src/routes/orders/orderController.ts
@@ -19,6 +19,7 @@ export async function createOrder(req: Request, res: Response) {
 
     const [newOrder] = await db
       .insert(ordersTable)
+      // @ts-ignore
       .values({ userId: userId })
       .returning();
 
