7c06db4eb6d026e8aa6bd0ad0d9a020d76ca98b8
EnterfaceLLC <dotenterface@gmail.com>
2024-11-03 13:56:06 -0600

    +Orders Fetch; Orders Page UI

---
+++ ecomm-dash/api/orders.ts
@@ -0,0 +1,31 @@
+//* API CONNECTION//
+import { API_URL } from "@/config";
+import { cookies } from "next/headers";
+
+//* FETCH ORDER FN//
+export async function fetchOrders() {
+  const token = cookies().get("token")?.value;
+
+  try {
+    const res = await fetch(`${API_URL}/orders`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token ?? "",
+      },
+      //   body: JSON.stringify({ order: {}, items }),
+    });
+    console.log("ORDERS API TOKEN",token)
+
+    if (!res.ok) {
+      console.log(res);
+      throw new Error("Failed to fetch orders");
+    }
+
+    const data = await res.json();
+    return data;
+
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
